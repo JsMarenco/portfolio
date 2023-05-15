8bb51bd1a55d1c089bfe8f2bc89825d210fae1a8
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-15 17:36:04 -0600

    Add function to fetch user repositories and add repository interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,3 +7,31 @@ export default function Home() {
     </div>
   )
 }
+
+export interface Repo {
+  owner: {
+    avatar_url: string
+  }
+  created_at: string
+  description: string
+  name: string
+  html_url: string
+  homepage?: string
+  topics: string[]
+}
+
+export const fetchRepos = async () => {
+  const res = await fetch('https://api.github.com/users/jsmarenco/repos')
+  const data = await res.json()
+
+  const Repos = data.filter((rawRepo: Repo) => {
+    if (rawRepo.description && rawRepo.description.includes(':0')) {
+      rawRepo.description = rawRepo.description.replace(':0', '')
+      return true
+    }
+
+    return false
+  })
+
+  return Repos
+}
