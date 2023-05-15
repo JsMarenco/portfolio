d5e6981475076ae48ce57b9c5a9e34153d478742
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-15 17:37:06 -0600

    Add hero to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
     <div className="max-w-[95%] mx-auto px-2">
       <Header />
+      <Hero />
     </div>
   )
 }
