3fd8c443e0f55a6ccdf1caf432b8705f771f30f6
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-15 15:51:11 -0600

    Add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router'
+
+export default function Header() {
+  const router = useRouter()
+
+  return (
+    <header className="w-full h-11 flex align-center justify-between p-1">
+      <div>
+        <span className="text-1xl" onClick={() => router.push('/')}>
+          JsMarenco
+        </span>
+      </div>
+
+      <nav className="flex align-middle justify-center gap-2">
+        <button
+          className="rounded-lg px-3 py-1 text-center text-black bg-white border border-black hover:bg-black hover:text-white"
+          onClick={() => router.push('/#projects')}
+        >
+          <span>Projects</span>
+        </button>
+
+        <button
+          className="rounded-lg px-3 py-1 text-center text-white bg-black border border-black hover:bg-white hover:text-black"
+          onClick={() => router.push('/#contact')}
+        >
+          <span>Contact</span>
+        </button>
+      </nav>
+    </header>
+  )
+}
