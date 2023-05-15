908bc20d93a5645fdcc221bc64060af41f94f194
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-15 15:51:36 -0600

    Add header to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import Header from '@/components/Header'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="max-w-[95%] mx-auto px-2">
+      <Header />
+    </div>
+  )
 }
