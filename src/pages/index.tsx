05788f3be8599b9e584a148b0a187fdfa1b226e3
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-15 18:54:56 -0600

    Add About component to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import About from '@/components/About'
 
 export default function Home() {
   return (
     <div className="max-w-[95%] mx-auto px-2">
       <Header />
       <Hero />
+      <About />
     </div>
   )
 }
