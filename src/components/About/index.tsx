ff78b97c4edcc7ed931297e83f28499bb3f29eca
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-15 18:54:21 -0600

    Add About component

---
+++ src/components/About/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function About() {
+  return (
+    <div className="my-20 px-5 max-w-xl mx-auto">
+      <h6 className="text-2xl text-center font-bold mb-3 text-gray-500">
+        About
+      </h6>
+
+      <p className="text-sm font-bold text-gray-500">
+        Hello, I'm Angel Marenco, a full stack developer with two years of
+        experience. I specialize in ReactJS, NextJS, and TypeScript and enjoy
+        creating and maintaining web applications. When I'm not working, I enjoy
+        constantly improving my skills. In my free time, I'm happy to share my
+        expertise in both areas to help others. In the future, I plan to
+        continue expanding my programming knowledge.
+      </p>
+    </div>
+  )
+}
