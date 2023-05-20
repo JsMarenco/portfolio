959c81aef97c1fae7573cbdf3367dcefca015122
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-20 12:29:10 -0600

    Update: Added new styles to buttons

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -4,33 +4,27 @@ const twitter = 'https://twitter.com/JsMarenco'
 
 export default function Contact() {
   return (
-    <section id="contact" className="my-20 px-5 max-w-xl mx-auto">
-      <h6 className="text-2xl text-center font-bold mb-3 text-gray-500">
-        Contact
-      </h6>
-
-      <article className="flex align-middle justify-center">
-        <div
-          className="text-gray-600 border border-gray-600 w-12 mx-1 h-12 text-xl text-center p-2 rounded-full cursor-pointer hover:text-gray-300"
-          onClick={() => window.open(twitter, '_blank', 'noopener,noreferrer')}
-        >
-          <i className="fa-brands fa-twitter"></i>
-        </div>
-
-        <div
-          className="text-gray-600 border border-gray-600 w-12 mx-1 h-12 text-xl text-center p-2 rounded-full cursor-pointer hover:text-gray-300"
-          onClick={() => window.open(github, '_blank', 'noopener,noreferrer')}
-        >
-          <i className="fa-brands fa-github"></i>
-        </div>
-
-        <div
-          className="text-gray-600 border border-gray-600 w-12 mx-1 h-12 text-xl text-center p-2 rounded-full cursor-pointer hover:text-gray-300"
-          onClick={() => window.open(linkedin, '_blank', 'noopener,noreferrer')}
-        >
-          <i className="fa-brands fa-linkedin-in"></i>
-        </div>
+    <section id="contact" className="flex align-center justify-center">
+      <article
+        className="hover:animate-bounce transition ease-in-out duration-150 m-3 hover:text-gray-200 hover:bg-gray-800 hover:border-none hover:shadow-gray-800 hover:shadow-lg text-gray-600 border border-gray-600 w-12 mx-1 h-12 text-xl text-center p-2 rounded-full cursor-pointer"
+        onClick={() => window.open(twitter, '_blank', 'noopener,noreferrer')}
+      >
+        <i className="fa-brands fa-twitter"></i>
       </article>
-    </section>
+
+      <article
+        className="hover:animate-bounce transition ease-in-out duration-150 m-3 hover:text-gray-200 hover:bg-gray-800 hover:border-none hover:shadow-gray-800 hover:shadow-lg text-gray-600 border border-gray-600 w-12 mx-1 h-12 text-xl text-center p-2 rounded-full cursor-pointer"
+        onClick={() => window.open(github, '_blank', 'noopener,noreferrer')}
+      >
+        <i className="fa-brands fa-github"></i>
+      </article>
+
+      <article
+        className="hover:animate-bounce transition ease-in-out duration-150 m-3 hover:text-gray-200 hover:bg-gray-800 hover:border-none hover:shadow-gray-800 hover:shadow-lg text-gray-600 border border-gray-600 w-12 mx-1 h-12 text-xl text-center p-2 rounded-full cursor-pointer"
+        onClick={() => window.open(linkedin, '_blank', 'noopener,noreferrer')}
+      >
+        <i className="fa-brands fa-linkedin-in"></i>
+      </article>
+    </section >
   )
 }
