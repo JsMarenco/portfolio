6507c53df44583c96ad7d84c9d639c725ebd80b0
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-22 10:40:24 -0600

    Add inner container, update text colors, and improve responsiveness

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,30 +4,32 @@ import Contact from "../Contact";
 /* eslint-disable react/no-unescaped-entities */
 export default function About() {
   return (
-    <section className="w-full h-screen snap-center flex align-center justify-center flex-col gap-3 py-4">
-      <h6 className="text-2xl text-center font-bold text-gray-700 mx-auto">
-        About
-      </h6>
+    <section className="w-full h-screen overflow-auto scrollbar-hide block sm:flex items-center justify-center snap-center py-4">
+      <div className="flex items-center justify-center flex-col gap-4">
+        <h6 className="text-2xl text-center font-bold text-black mx-auto">
+          About
+        </h6>
 
-      <section className="w-[250px] h-[250px] rounded-xl mx-auto overflow-hidden shadow-md shadow-gray-700">
-        <Image
-          src="https://avatars.githubusercontent.com/u/65215919?v=4"
-          alt="Angel Marenco | JsMarenco avatar's"
-          width={250}
-          height={250}
-        />
-      </section>
+        <section className="w-[250px] h-[250px] flex-shrink-0 rounded-xl mx-auto overflow-hidden shadow-md shadow-gray-700">
+          <Image
+            src="https://avatars.githubusercontent.com/u/65215919?v=4"
+            alt="Angel Marenco | JsMarenco avatar's"
+            width={250}
+            height={250}
+          />
+        </section>
 
-      <p className="text-sm text-gray-500 font-semibold text-center sm:max-w-[50%] max-w-[90%] mx-auto">
-        Hello, I'm Angel Marenco, a full stack developer with two years of
-        experience. I specialize in ReactJS, NextJS, and TypeScript and enjoy
-        creating and maintaining web applications. When I'm not working, I enjoy
-        constantly improving my skills. In my free time, I'm happy to share my
-        expertise in both areas to help others. In the future, I plan to
-        continue expanding my programming knowledge.
-      </p>
+        <p className="text-black font-medium leading-6 text-center max-w-[90%] sm:max-w-[650px] mx-auto">
+          Hello, I'm Angel Marenco, a full stack developer with two years of
+          experience. I specialize in ReactJS, NextJS, and TypeScript and enjoy
+          creating and maintaining web applications. When I'm not working, I enjoy
+          constantly improving my skills. In my free time, I'm happy to share my
+          expertise in both areas to help others. In the future, I plan to
+          continue expanding my programming knowledge.
+        </p>
 
-      <Contact />
+        <Contact />
+      </div>
     </section>
   )
 }
